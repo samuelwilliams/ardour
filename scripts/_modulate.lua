be985f525bc1f378eb1e02ecf7965016286d77c5
Robin Gareus <robin@gareus.org>
2017-02-23 03:20:43 +0100

    tweak example to add a modulation-script using a script

--- scripts/_modulate.lua
+++ scripts/_modulate.lua
@@ -10,12 +10,14 @@ function factory () return function ()
 	function dsp_modulate(ctrl, bufs, n_samples, offset, start)
 		cnt = cnt or 0
 		cnt = cnt + n_samples
-		if cnt > 48000 then cnt = cnt - 48000 end
-		local gain = ctrl:at (Evoral.Parameter (ARDOUR.AutomationType.PluginAutomation, 0, 3)):to_automationcontrol():to_plugincontrol()
-		assert (gain)
-		local a = ARDOUR.DSP.fast_coefficient_to_dB (ARDOUR.DSP.compute_peak(bufs:get_audio(0):data(offset), n_samples, 0))
+		local sr = Session:nominal_frame_rate ()
+		if cnt > sr then cnt = cnt - sr end
+		--local gain = ctrl:at (Evoral.Parameter (ARDOUR.AutomationType.PluginAutomation, 0, 3)):to_automationcontrol():to_plugincontrol()
+		--assert (gain)
+		local a = ARDOUR.DSP.fast_coefficient_to_dB (ARDOUR.DSP.compute_peak(bufs:get_audio(0):data(offset), n_samples, 0)) + 10
 		a = math.min (20, math.max(-20, a))
-		gain:modulate_to (5 * math.sin (math.pi * cnt / 48000) - a)
+		--gain:modulate_to (5 * math.sin (math.pi * cnt / sr) - a)
+		ARDOUR.LuaAPI.set_control(ctrl, 3, 5 * math.sin (math.pi * cnt / sr) - a)
 	end
 	]]
 	))
